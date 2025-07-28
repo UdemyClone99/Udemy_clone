7162d9095e2e38b0c8fc6c78f66dc4069b552263
ghyl098 <ghyalpolama62@gmail.com>
2025-07-28 08:04:45 +0545

    Hero Section by Ghylpo

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+      <div>
+        
+      </div></div>
+    </div>
   )
 }
 
