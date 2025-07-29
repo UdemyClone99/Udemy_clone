50f5ed364f93e3cecb0e4cfef17eaea12557f5ba
MilanMoktan99 <milanmok816@gmail.com>
2025-07-29 18:12:45 +0545

    course details and loading animation done by milan

--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -12,7 +12,7 @@ const CoursesSection = () => {
       <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br/>business and wellness, our courses are crafted to deliver results.</p>
 
 
-<div className='grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
+<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 md:px-0 my-10 md:my-16'>
   {allCourses.slice(0,4).map((course, index) => <CourseCard key={index} course={course}/>)}
 </div>
 
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState, } from 'react'
 import { dummyCourses } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext();
 
@@ -16,12 +17,38 @@ export const AppContextProvider = (props) => {
         setAllCourses(dummyCourses)
     }
 
+    const calculateChaptertime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration);
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]});
+    }
+
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]});
+    }
+
+    const calculteNoOfLecture = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
+    }
+
     useEffect(() => {
         fetchAllCourses()
     },[])
 
     const value = {
-        currency, allCourses, navigate
+        currency, allCourses, navigate,
+        calculateChaptertime, calculateCourseDuration, calculteNoOfLecture,
 
     }
 
