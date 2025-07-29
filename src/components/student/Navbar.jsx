57051709ae33f3a4d1b86a8389e8dca7f43400d5
ghyl098 <ghyalpolama62@gmail.com>
2025-07-29 07:57:31 +0545

    Course card by ghylpo

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
     <div>
-      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
-      <div className='hidden md:flex items-center gap-5 text-gray-500'>
-      <div>
-        
-      </div></div>
+      Navbar
     </div>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { createContext } from 'react'
+import { createContext, useEffect, useState, } from 'react'
+import { dummyCourses } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate
+
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch All Courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(() => {
+        fetchAllCourses()
+    },[])
+
     const value = {
+        currency, allCourses, navigate
 
     }
+
     return (
         <AppContext.Provider value={value}>
             {props.children}
